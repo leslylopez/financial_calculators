a1a67c45065110021230e1e9cdc47181392d9e4e
Lesly Lopez <leslylpz32@gmail.com>
2024-04-29 17:09:05 -0500

    completed mortageCalculator.html and mortgageCalc.js

---
+++ scripts/basicCalc.js
@@ -0,0 +1,72 @@
+"use strict"
+console.log("it is working!");
+
+window.onload = init;
+
+function init(){
+
+    //get the buttons from the page so we can work with them in JS
+    const addButton = document.querySelector("#addButton");
+    const subtractButton = document.querySelector("#subtractButton");
+    const multiplyButton = document.querySelector("#multiplyButton");
+    const divideButton = document.querySelector("#divideButton");
+
+    //wire up the functions to the click of the buttons
+    addButton.addEventListener("click", add);
+    subtractButton.addEventListener("click", subtract);
+    multiplyButton.addEventListener("click", multiply);
+    divideButton.addEventListener("click", divide);
+
+}
+
+//create an add function that gets num1 and num2 of the page and sums them
+function add(){
+
+    //get the values out of the input fields the user types in
+    let number1 = Number( document.querySelector("#number1Field").value );
+    let number2 = Number( document.querySelector("#number2Field").value );
+
+    //do the math
+    let result = number1 + number2;
+
+    //put the result in the answer input field
+    document.querySelector("#answerField").value = result;
+}
+
+//create a subtract function that gets num1 and num2 of the page and subttracts them
+function subtract(){
+
+    //get the values out of the input fields the user types in
+    let number1 = Number( document.querySelector("#number1Field").value );
+    let number2 = Number( document.querySelector("#number2Field").value );
+
+    //do the math
+    let result = number1 - number2;
+
+    //put the result in the answer input field
+    document.querySelector("#answerField").value = result;
+}
+
+function multiply () {
+    //get the values out of the input fields the user types in
+    let number1 = Number(document.querySelector("#number1Field").value);
+    let number2 = Number(document.querySelector("#number2Field").value);
+
+    //add those numbers together
+    let result = number1 * number2;
+
+    //put the result in the answer input field
+    document.querySelector("#answerField").value = result;
+}
+
+function divide() {
+    //get the values out of the input fields the user types in
+    let number1 = Number(document.querySelector("#number1Field").value);
+    let number2 = Number(document.querySelector("#number2Field").value);
+
+    //add those numbers together
+    let result = number1 / number2;
+
+    //put the result in the answer input field
+    document.querySelector("#answerField").value = result;
+}
---
+++ scripts/mortgageCalc.js
@@ -0,0 +1,34 @@
+"use strict"
+console.log("it is working!");
+
+window.onload = init;
+
+function init() {
+
+    //get the button from the page so we can work with them in JS
+    const calculateButton = document.querySelector("#calculateButton");
+
+    calculateButton.addEventListener("click", calculate);
+
+}
+
+
+//input
+function calculate() {
+
+let loanAmount = Number (document.querySelector("#loanAmountField").value);
+let interestRate = Number (document.querySelector("#interestRateField").value);
+let loanTerm = Number (document.querySelector("#loanTermField").value);
+let monthlyInterestRate = interestRate/12/100
+let numberOfPayments = loanTerm * 12
+
+//do the math
+
+let monthlyPayment = loanAmount* (monthlyInterestRate * Math.pow(1+monthlyInterestRate, numberOfPayments )/ (Math.pow(1+monthlyInterestRate, numberOfPayments )-1))
+let totalPayment = monthlyPayment * numberOfPayments;
+let totalInterest = totalPayment - loanAmount;
+
+let message =  "Your monthly payment is " + monthlyPayment.toFixed(2) + " for " + loanTerm + " years. Your total payment is " + totalPayment.toFixed(2) + " and total interest is " + totalInterest.toFixed(2);
+document.querySelector ("#calculateMessage").innerHTML =message
+
+}
